a2413441629c3dff13c5d59348bf50df1513b4a3
Pea <m.a.tyczynska@gmail.com>
2016-10-18 18:50:03 +0000

    Chapter 05 done

---
+++ chap05/ex04_gets-chomp2.rb
@@ -0,0 +1,9 @@
+puts "Hello there, and what\'s your first name?"
+first_name = gets.chomp
+puts "...and what\'s your middle name?"
+middle_name = gets.chomp
+puts "...and finally, what\'s your last name?"
+last_name = gets.chomp
+puts "Your name is #{first_name} #{middle_name} #{last_name}? What a lovely name!"
+
+puts "Pleased to meet you, #{first_name} #{middle_name} #{last_name}. :)"
---
+++ chap05/ex05_fav-num.rb
@@ -0,0 +1,4 @@
+puts "What's your favourite number?"
+favnum = gets.chomp.to_i
+favnum2 = favnum + 1
+puts "That's a very nice number. Now how about #{favnum2}? It's even bigger and better than #{favnum}! :)"
